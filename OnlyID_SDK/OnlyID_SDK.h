6b669e4bf8e237a4547668d96dac5d3dcf94be6d
ltb <liangtb@qq.com>
2017-09-16 15:09:06 +0800

    Initial Commit

---
+++ OnlyID_SDK/OnlyID_SDK.h
@@ -0,0 +1,19 @@
+//
+//  OnlyID_SDK.h
+//  OnlyID_SDK
+//
+//  Created by 梁庭宾 on 2017/9/16.
+//  Copyright © 2017年 onlyID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OnlyID_SDK.
+FOUNDATION_EXPORT double OnlyID_SDKVersionNumber;
+
+//! Project version string for OnlyID_SDK.
+FOUNDATION_EXPORT const unsigned char OnlyID_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnlyID_SDK/PublicHeader.h>
+
+
